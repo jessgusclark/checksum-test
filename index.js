d7c2a06d0aa6221b8ecd3e86ddf316bb48618c97
Jesse Clark <hello@developerjesse.com>
2020-04-03 14:51:36 +0300

    Passing null instead of chainID 1 works.

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const ethAddress = '0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5';
 console.log(ethAddress);
 console.log(rskjs.toChecksumAddress(ethAddress, 1));
 console.log(ethAddress === rskjs.toChecksumAddress(ethAddress, 1));
+console.log(ethAddress === rskjs.toChecksumAddress(ethAddress, null));
